75cd385504fbe23e440354f03d4a49d9e75ac164
Alex Wan <a_wanderous@hotmail.com>
2019-10-11 16:51:05 +1100

    game logic set up

--- index.js
+++ index.js
@@ -13,16 +13,23 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 // words in array
 var heroes = ["hawkman", "aquaman", "batman", "superman", "wonder woman", "green arrow", "captain america", "daredevil", "falcon", "hulk", "ironman", "thor", "robin", "storm", "war machine", "doctor strange", "spiderman"];
 
+// random hero generator
+var theHero = heroes[Math.floor(Math.random() * heroes.length)];
+var chosenHero = new Word(theHero);
+
 // guessed letters in array
 var correctLetters = [];
 var incorrectLetters = [];
+var solved = false;
 
+//generate new word
 function newHero() {
     var theHero = heroes[Math.floor(Math.random() * heroes.length)];
-    var chosenHero = new Word(theHero);
-
-    var solved = false;
+    chosenHero = new Word(theHero);
+    correctLetters = [];
+    incorrectLetters = [];
     guessCount = 10;
+    var solved = false;
 };
 
 function play() {
@@ -31,33 +38,93 @@ function play() {
         newHero();
     };
 
+    //test if letter correct
     var wordDone = [];
-    chosenHero.hero.forEach(spellCheck);
+    chosenHero.hero.forEach(checkDone);
 
+    // letters left to guess
     if (wordDone.includes(false)) {
 
         inquirer
             .prompt([
-                type = "input",
-                message = "Guess a letter in the word.",
-                name = "guesses"
-            ]).then(function (input) {
+                {
+                type: "input",
+                message: "Guess a letter in the word.",
+                name: "guesses"
+            }]).then(function (input) {
 
+                //check if input correctly
                 if (!letters.includes(input.guesses) || input.guesses.length > 1 || input.guesses === "") {
                     console.log("\n Please enter a letter.\n");
                     play();
                 } else {
+
+                    //check if entered already
                     if (incorrectLetters.includes(input.guesses) || correctLetters.includes(input.guesses)) {
                         console.log("\n This letter has already been guessed\n");
                         play();
-                    }
+                    } else {
+
+                        // check if guess is correct
+                        var spellCheck = [];
+                        chosenHero.userGuess(input.guesses);
+                        chosenHero.hero.forEach(spelling);
+
+                        if (spellCheck.join('') === wordDone.join('')) {
+                            console.log("\nThat was incorrect...\n");
+                            incorrectLetters.push(input.guesses);
+                            guessCount--;
+                        } else {
+                            console.log("\nThat's correct!\n");
+                            correctLetters.push(input.guesses);
+                        };
+                    };
+                };
+                // score update                
+                chosenHero.log();
+                console.log("Guesses left: " + guessCount);
+                console.log("Letters Guessed: " + incorrectLetters.join(" ") + "\n");
+
+                if (guessCount > 0) {
+                    play();
+                } else {
+                    losses++;
+                    console.log("Sorry, you lose...\n");
+                    console.log("Score is:\nWins: " + wins + "\nLoses: " + losses);
+                    reset();
                 }
 
-            })
+                function spelling(key) {
+                    spellCheck.push(key.guessed)
+                };
 
+            });
+
+    } else {
+        wins++;
+        console.log("You Win!!\n");
+        console.log("Score is:\nWins: " + wins + "\nLoses: " + losses);
+        reset();
+    }
+
+    function checkDone(key) {
+        wordDone.push(key.guessed);
     };
-
 };
 
-newHero();
+function reset() {
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "Would you like to play again?",
+            choices: ["Play again", "Exit"],
+            name: "reset"
+        }]).then(function (input) {
+            if (input.reset === "Play again") {
+                solved = true;
+                newHero();
+            }
+        })
+}
+
 play();
