3bcb83b144e29800534fb78890d0752cc37b4ad1
Alex Wan <a_wanderous@hotmail.com>
2019-10-11 14:19:47 +1100

    added picking hero function

--- index.js
+++ index.js
@@ -5,18 +5,33 @@ var guessCount = 10;
 
 
 // letters in array
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // words in array
-var words = ["hawkman","aquaman","batman","superman","wonder woman", "green arrow", "captain america", "daredevil", "falcon", "hulk", "ironman", "thor", "robin", "storm", "war machine", "doctor strange", "spiderman"];
+var heroes = ["hawkman", "aquaman", "batman", "superman", "wonder woman", "green arrow", "captain america", "daredevil", "falcon", "hulk", "ironman", "thor", "robin", "storm", "war machine", "doctor strange", "spiderman"];
 
-var theHero = words[Math.floor(Math.random() * words.length)];
+// guessed letters in array
+var correctLetters = [];
+var incorrectLetters = [];
 
+
+function newHero() {
+
+    if (solved) {
+        var theHero = heroes[Math.floor(Math.random() * heroes.length)];
+        var chosenHero = new Word(theHero);
+
+        var solved = false;
+        guessCount = 10;
+    }
+}
 
 inquirer
-.prompt([
-    type = "input",
-    message = "guess a letter in the word."
-    name = "guesses"
-  ])
- 
+    .prompt([
+        type = "input",
+        message = "guess a letter in the word.",
+        name = "guesses"
+    ])
+
+
+newHero();
--- word.js
+++ word.js
@@ -1,1 +1,9 @@
 var Letter = require("./letter");
+
+function Word(){
+
+}
+
+
+
+module.exports = Word;
